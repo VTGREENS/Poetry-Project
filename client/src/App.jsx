791f2a9f79d80966ae98467b63697baf1214d16a
stephen-reed-1221 <stephen.reed.1221@gmail.com>
2023-01-23 16:42:13 -0500

    reset app back to normal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,7 @@
-import SignUp from "./components/auth/signup/Signup";
-import SignIn from "./components/auth/login/Login";
-import PermanentDrawerLeft from "./components/sidebars/sidebar-left/SidebarLeft";
-import PermanentDrawerRight from "./components/sidebars/sidebar-right/SidebarRight";
-import StickyFooter from "./components/footer/StickyFooter";
+
 
 function App() {
-  return (
-    <>
-    <SignIn />
-    <SignUp />
-    <PermanentDrawerLeft />
-    <PermanentDrawerRight />
-    <StickyFooter />
-    </>
-    
-  );
+  return ( );
 }
 
 export default App;
